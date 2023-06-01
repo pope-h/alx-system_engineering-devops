3b04b5b67039c5598c7445999503998fe566a574
Ekarika Nsemeke <hamatthan@gmail.com>
2023-06-01 16:39:41 +0100

    corrected task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-"""
-Script that uses JSONPlaceholder API to get information about an employee
-"""
-
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
-from sys import argv
-
-
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-
-    employee_response = requests.get(employee_url)
-    employee_response.raise_for_status()
-    employee_data = employee_response.json()
-    employee_name = employee_data["name"]
-
-    todo_response = requests.get(todo_url)
-    todo_response.raise_for_status()
-    todo_data = todo_response.json()
-    total_tasks = len(todo_data)
-    done_tasks = sum(task["completed"] for task in todo_data)
-
-    print("Employee", employee_name, "is done with tasks",
-          "({}/{})".format(done_tasks, total_tasks) + ":")
-
-    for task in todo_data:
-        if task["completed"]:
-            print(f"\t{task['title']}")
+import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(argv[1])
-    get_employee_todo_progress(employee_id)
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
+
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
