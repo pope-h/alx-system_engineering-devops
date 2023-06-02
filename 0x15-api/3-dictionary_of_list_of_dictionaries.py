371710ae0d944250765644b593e3ba81cce22ce1
Ekarika Nsemeke <hamatthan@gmail.com>
2023-06-02 08:18:12 +0100

    all workers to json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = r.json()
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = r.json()
+
+    export_data = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        user_tasks = [task for task in tasks if task.get('userId') == user_id]
+        export_data[user_id] = [{
+            'username': username,
+            'task': task.get('title'),
+            'completed': task.get('completed')
+        } for task in user_tasks]
+
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as f:
+        json.dump(export_data, f)
